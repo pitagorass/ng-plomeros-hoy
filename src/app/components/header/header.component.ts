e346d7b216e73c58792f9229848118f99df8a52f
Santiago <prv_santiago.vargas@dale.com.co>
2023-05-09 18:15:50 -0500

    add structure

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  template: `
+    <header class="audiowide-font">
+      <!-- Contenido del encabezado -->
+    </header>
+  `,
 })
 export class HeaderComponent {
 
